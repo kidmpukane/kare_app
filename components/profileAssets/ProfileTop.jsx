226161b9c2abce76ba064a0af35168b5dff97423
kidmpukane <lwandisozwane91@gmail.com>
2025-02-01 15:41:25 +0200

    refactor ProfileTop component to include PressableNav and adjust layout styles

--- components/profileAssets/ProfileTop.jsx
+++ components/profileAssets/ProfileTop.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import HeaderBody from '../../molecules/HeaderBody';
+import PressableNav from '../../molecules/PressableNav';
 import React from 'react';
 
 const profileImage = { uri: 'https://images.unsplash.com/photo-1531727991582-cfd25ce79613?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' };
@@ -11,6 +12,7 @@ const ProfileTop = () => {
         <View style={styles.overlay} />
         <View style={styles.textContainer}>
           <HeaderBody/>
+          <PressableNav/>
         </View>
       </ImageBackground>
     </View>
@@ -21,34 +23,18 @@ export default ProfileTop;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    padding: 10,
   },
   profileImageStyles: {
-    height: 300,
+    height: 500,
     justifyContent: 'flex-end', // Align text at the bottom
   },
   imageBorder: {
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0,
-    borderBottomLeftRadius: 0,
+    borderRadius: 20,
   },
-  overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: 'rgba(0, 0, 0, 0.4)', // Dark overlay
-    borderTopLeftRadius: 30,
-  },
+
   textContainer: {
-    padding: 15,
+    padding: 10,
   },
-  title: {
-    color: '#FFFFFF',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 5,
-  },
-  subtitle: {
-    color: '#FFFFFF',
-    fontSize: 14,
-  },
+
 });

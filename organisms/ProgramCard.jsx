f14c9b900429477fa3db4ff9c21d12697ecc6074
kidmpukane <lwandisozwane91@gmail.com>
2025-02-05 18:24:01 +0200

    refactor ProgramCard component to enhance layout with duration and results indicators

--- organisms/ProgramCard.jsx
+++ organisms/ProgramCard.jsx
@@ -5,6 +5,19 @@ import React from "react";
 const ProgramCard = () => {
   return (
     <View>
+      <View style={styles.cardTopContainer}>
+        <View style={styles.durationIndicator}>
+          <View style={styles.durationContainer}>
+            <Text style={styles.durationText}>Duration</Text>
+          </View>
+          <View style={styles.weeksContainer}>
+            <Text>12 Weeks</Text>
+          </View>
+        </View>
+        <View style={styles.resultsIndicator}>
+          <Text>Dry</Text>
+        </View>
+      </View>
       <View style={styles.cardDescription}>
         <HeaderBody2 />
       </View>
@@ -15,6 +28,11 @@ const ProgramCard = () => {
 export default ProgramCard;
 
 const styles = StyleSheet.create({
+  cardTopContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
   cardDescription: {
     paddingTop: 20,
     paddingBottom: 20,
@@ -24,5 +42,44 @@ const styles = StyleSheet.create({
     width: "100%",
     height: 240,
     borderRadius: 40,
+  },
+  resultsIndicator: {
+    padding: 5,
+    borderWidth: 1.5,
+    borderColor: "#24221E",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "pink",
+    width: "20%",
+  },
+  durationIndicator: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#EBE5DC",
+    padding: 0,
+    borderWidth: 1.5,
+    borderColor: "#24221E",
+    borderRadius: 30,
+    width: "40%",
+  },
+  durationContainer: {
+    backgroundColor: "#24221E",
+    color: "#EBE5DC",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    width: "50%",
+  },
+  weeksContainer: {
+    color: "#EBE5DC",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 30,
+    width: "50%",
+  },
+  durationText: {
+    color: "#EBE5DC",
   },
 });

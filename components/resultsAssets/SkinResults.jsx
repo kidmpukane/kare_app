1b3cc6a3cee4a5e8b44fb64b58e35ee594c1ce4d
kidmpukane <lwandisozwane91@gmail.com>
2025-02-05 15:53:10 +0200

    refactor SkinResults component for improved layout and styling, adding results container and indicator

--- components/resultsAssets/SkinResults.jsx
+++ components/resultsAssets/SkinResults.jsx
@@ -5,18 +5,20 @@ import React from "react";
 
 const SkinResults = () => {
   return (
-    <View>
+    <View style={styles.resultsContainer}>
       <View style={styles.resultsTopContainer}>
+        <View style={styles.resultsIndicator}>
+          <Text>Results</Text>
+        </View>
         <View>
           <HeaderBody2 />
         </View>
-        <View>
-          <Text>Results</Text>
-        </View>
       </View>
       <View style={styles.underline} />
-      <PressableNav2 />
-      <PressableNav2 />
+      <View style={styles.resultsBottomContainer}>
+        <PressableNav2 />
+        <PressableNav2 />
+      </View>
     </View>
   );
 };
@@ -24,16 +26,36 @@ const SkinResults = () => {
 export default SkinResults;
 
 const styles = StyleSheet.create({
+  resultsContainer: {
+    padding: 10,
+    borderWidth: 1.5,
+    borderColor: "#24221E",
+    borderRadius: 30,
+  },
+  resultsIndicator: {
+    position: "absolute", // Use absolute positioning
+    top: 10, // Distance from the top of the parent container
+    right: 10, // Distance from the right of the parent container
+    padding: 5,
+    borderWidth: 1.5,
+    borderColor: "#24221E",
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "pink",
+    width: "20%",
+  },
   resultsTopContainer: {
-    flexDirection: "row",
+    display: "flex",
     justifyContent: "space-between",
   },
   underline: {
-    height: 1,
+    height: 1.5,
     backgroundColor: "black",
     width: "90%",
     alignSelf: "center",
     marginTop: 20,
     marginBottom: 20,
   },
+  resultsBottomContainer: {},
 });

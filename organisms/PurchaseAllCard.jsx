b3a80ac885c7b6eb2c26e243b68249e4575976f4
kidmpukane <lwandisozwane91@gmail.com>
2025-02-08 18:34:09 +0200

    feat: add ProductCard and PurchaseAllCard components

---
+++ organisms/PurchaseAllCard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PurchaseAllCard = () => {
+  return (
+    <View>
+      <Text>PurchaseAllCard</Text>
+    </View>
+  );
+};
+
+export default PurchaseAllCard;
+
+const styles = StyleSheet.create({});

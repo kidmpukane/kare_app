7d4215bada623936541b3b9ffe3c5acb6fce50fc
kidmpukane <lwandisozwane91@gmail.com>
2025-02-12 09:53:04 +0200

    feat: add SurveyForm component with basic structure

---
+++ forms/SurveyForm.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const SurveyForm = () => {
+  return (
+    <View>
+      <Text>SurveyForm</Text>
+    </View>
+  );
+};
+
+export default SurveyForm;
+
+const styles = StyleSheet.create({});

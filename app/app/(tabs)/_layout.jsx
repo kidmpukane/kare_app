f510642110143a8c93ca934406ac49801cf79266
zossimov <zossimov@email.com>
2025-02-25 14:53:59 +0200

    refactor: enhance tab layout styling and update screen options for better UI

--- app/app/(tabs)/_layout.jsx
+++ app/app/(tabs)/_layout.jsx
@@ -1,9 +1,30 @@
 import { Tabs } from "expo-router";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { BlurView } from 'expo-blur';
+import { StyleSheet } from 'react-native';
+
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs   
+    screenOptions={{
+      tabBarStyle: {
+        position: 'absolute',
+        bottom: 10,
+        left: 20,
+        right: 20,
+        elevation: 0,
+        backgroundColor: '#24221E',
+        borderRadius: 15,
+        height: 70,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 10 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        borderWidth: 1,
+        borderColor: '#ffffff'
+      }
+    }}> 
       <Tabs.Screen
         name="products"
         options={{
@@ -31,8 +52,9 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="home"
         options={{
-          title: "Home",
-          tabBarIcon: () => <FontAwesome name="home" />,
+          title: "Homes",
+          headerShown: false,
+          tabBarIcon: () => <FontAwesome name="home" color="white" />,
         }}
       />
 
@@ -40,14 +62,16 @@ export default function TabsLayout() {
         name="results"
         options={{
           title: "Results",
-          tabBarIcon: () => <FontAwesome name="pie-chart" />,
+          headerShown: false,
+          tabBarIcon: () => <FontAwesome name="pie-chart" color="white" />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: "Profile",
-          tabBarIcon: () => <FontAwesome name="user" />,
+          headerShown: false,
+          tabBarIcon: () => <FontAwesome name="user" color="white" />,
         }}
       />
     </Tabs>
--- app/app/recommendations/_layout.jsx
+++ app/app/recommendations/_layout.jsx
@@ -3,9 +3,10 @@ import { Stack } from "expo-router";
 export default function ProgramsLayout() {
   return (
     <Stack>
-      <Stack.Screen name="resultsIndex" options={{ title: "ResultsIn" }} />
+      <Stack.Screen name="resultsIndex"  options={{ headerShown: false , title: "nothing"}} />
       <Stack.Screen name="routines" options={{ title: "Routines" }} />
-      <Stack.Screen name="programs" options={{ title: "Programs" }} />
+      <Stack.Screen name="programs" options={{ title: "Programs" }}  />
     </Stack>
   );
 }
+    
